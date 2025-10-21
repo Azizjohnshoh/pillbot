92eccbdc0cfea8d51b7aae225d4446b2eb40f304
Azizjohnshoh <142935806+Azizjohnshoh@users.noreply.github.com>
2025-10-21 07:36:50 -0400

    Update bot_handlers.py

--- bot_handlers.py
+++ bot_handlers.py
@@ -1,4 +1,3 @@
-
 import asyncio, re
 from datetime import datetime
 from utils import dbmod, ui, lang
@@ -19,7 +18,7 @@ async def handle_message(update, send_message, send_voice):
         meds = await dbmod.list_reminders_for_chat(chat_id)
         T = lang.TEXT.get((await dbmod.get_user_prefs(chat_id))[0], lang.TEXT['uz'])
         if not meds:
-            await send_message(chat_id, T['greeting'] + "\\n" + T['start_menu'], reply_markup=ui.main_menu(T and 'uz' or 'uz'))
+            await send_message(chat_id, T['greeting'] + "\n" + T['start_menu'], reply_markup=ui.main_menu('uz'))
             if (await dbmod.get_user_prefs(chat_id))[1] and send_voice:
                 await send_voice(chat_id, T['greeting'], lang_code=(await dbmod.get_user_prefs(chat_id))[0])
         else:
@@ -44,7 +43,7 @@ async def handle_message(update, send_message, send_voice):
     # if user is in a state waiting for custom time/name etc - defer to dbmod.get_state
     state, temp = await dbmod.get_state(chat_id)
     if state == "awaiting_custom_time":
-        if re.match(r'^(?:[01]\\d|2[0-3]):[0-5]\\d$', text):
+        if re.match(r'^(?:[01]\d|2[0-3]):[0-5]\d$', text):
             title = temp.get("title") if temp else "NoName"
             time_str = text
             await dbmod.add_reminder(chat_id, title, time_str, "once")
@@ -62,37 +61,39 @@ async def handle_message(update, send_message, send_voice):
     if text:
         await send_message(chat_id, "Echo: " + text)
 
+
 async def handle_callback(callback, send_message, send_voice):
     cq = callback
     data = cq.get("data","")
     msg = cq.get("message",{})
     chat_id = msg.get("chat",{}).get("id")
     T = lang.TEXT.get((await dbmod.get_user_prefs(chat_id))[0], lang.TEXT['uz'])
-    log_prefix = f"[Callback:{data}]"
 
-    # Debug log message by sending to admin (optional) - avoid spamming general users
     # Main menu
     if data == "add_med":
         await dbmod.set_state(chat_id, "awaiting_med_name", {})
         await send_message(chat_id, T['ask_med_name'])
         return
+
     if data == "my_meds":
         meds = await dbmod.list_reminders_for_chat(chat_id)
         if not meds:
             await send_message(chat_id, T['no_meds'])
         else:
             for r in meds:
-                await send_message(chat_id, f\"{r['id']}: {r['title']} @ {r['time']} [{r.get('recurring')}]\" )
+                await send_message(chat_id, f"{r['id']}: {r['title']} @ {r['time']} [{r.get('recurring')}]")
         return
+
     if data == "report":
         meds = await dbmod.list_reminders_for_chat(chat_id)
         await send_message(chat_id, T['report'].format(total=len(meds)))
         return
+
     if data == "settings":
-        await send_message(chat_id, T['settings'], reply_markup=ui.settings_menu((await dbmod.get_user_prefs(chat_id))[0], voice_on=bool((await dbmod.get_user_prefs(chat_id))[1])) )
+        await send_message(chat_id, T['settings'], reply_markup=ui.settings_menu((await dbmod.get_user_prefs(chat_id))[0], voice_on=bool((await dbmod.get_user_prefs(chat_id))[1])))
         return
 
-    # time chosen from inline buttons: time_ HH:MM
+    # time chosen from inline buttons: time_HH:MM
     if data.startswith("time_"):
         time_chosen = data.split("_",1)[1]
         state, temp = await dbmod.get_state(chat_id)
